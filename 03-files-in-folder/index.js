bfd6610c08a7c072cbaa650ad1bfb92b6433ef3d
Alexander Golubovich <verrucktx@gmail.com>
2022-05-24 22:47:27 +0300

    fix: fix minor bug
    add units in 3rd task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -10,8 +10,7 @@ const path = require('path');
                 let extName = path.extname(dirObj.name);
                 let fileName = path.basename(dirObj.name, extName);
                 const fileStat = await fs.promises.stat(path.resolve(folderPath, dirObj.name));
-                console.log(`${fileName} - ${extName.slice(1)} - ${fileStat.size}`);
-                
+                console.log(`${fileName} - ${extName.slice(1)} - ${fileStat.size} bytes`);
             }
         }
     }
